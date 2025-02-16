c0a22c834c05f93503183998f05b08c9b29a66ae
BugHunter-X <askzenai@gmail.com>
2025-02-16 07:08:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Some Tailwind CSS classes are not applied in the component
+<div class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Button
+</div>
+```
